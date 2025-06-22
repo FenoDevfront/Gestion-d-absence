b4c7f5ee028e69b8ac80ebb0c2871da0fd179f49
Feno Niaina Ratsimbaharisoa <sabmistar@gmail.com>
2025-06-22 10:52:36 +0300

    modified:   resources/views/layout.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AbsenceController;
+use App\Http\Controllers\CongeController;
+use App\Http\Controllers\RetardController;
 
 Route::resource('absences', AbsenceController::class);
 Route::resource('conges', CongeController::class);
