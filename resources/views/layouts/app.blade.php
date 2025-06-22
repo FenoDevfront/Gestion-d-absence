97f887f7b899bb468803f44fc8357233f82b1bb2
Feno Niaina Ratsimbaharisoa <sabmistar@gmail.com>
2025-06-22 17:07:23 +0300

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion du Personnel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="https://tsarajoro.dev/wp-content/uploads/2024/09/cropped-Logo-270x270.png" type="image/png">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">Gestion Personnel</a>
+            <a class="navbar-brand" href="{{ url('/') }}" style="width: 20%;"><img src="https://tsarajoro.dev/wp-content/uploads/2024/09/Logo.png" alt="Tsarajoro" style="width: 100%; height: auto;"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
