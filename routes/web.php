3e0faac910261512fa6042d1ccb6863bdf4a0623
Feno Niaina Ratsimbaharisoa <sabmistar@gmail.com>
2025-06-22 15:34:04 +0300

    deleted:    database/migrations/2025_06_22_100606_add_google_id_to_users_table.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,25 @@
 <?php
 
+// Dans routes/web.php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AbsenceController;
 use App\Http\Controllers\CongeController;
 use App\Http\Controllers\RetardController;
+use App\Http\Controllers\Auth\GoogleController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::resource('absences', AbsenceController::class);
-Route::resource('conges', CongeController::class);
-Route::resource('retards', RetardController::class);
+// // Routes publiques
+// Route::get('/', function () {
+//     if (!Auth::check()) {
+//         return redirect('/auth/google');
+//     }
+//     return app(HomeController::class)->index();
+// })->name('home');
+
+// Routes protégées par authentification
+// Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::resource('absences', AbsenceController::class);
+    Route::resource('conges', CongeController::class);
+    Route::resource('retards', RetardController::class);
 
