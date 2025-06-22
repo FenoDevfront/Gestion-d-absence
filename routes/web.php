670761c08b430fb1d6aba67ff3c8fb8a31137e7b
Feno Niaina Ratsimbaharisoa <sabmistar@gmail.com>
2025-06-22 11:48:20 +0300

    modified:   resources/views/layout.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,8 @@ use App\Http\Controllers\RetardController;
 Route::resource('absences', AbsenceController::class);
 Route::resource('conges', CongeController::class);
 Route::resource('retards', RetardController::class);
-Route::get('/absences', [AbsenceController::class, 'index']);
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 
