d9b94f8f80b0468c779e701e14992bcc766aa2c8
Feno Niaina Ratsimbaharisoa <sabmistar@gmail.com>
2025-06-22 11:52:24 +0300

    modified:   resources/views/layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">Gestion Personnel</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
     
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('absences.index') }}">Absences</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('conges.index') }}">Congés</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('retards.index') }}">Retards</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container mt-4">
         @include('flash-message')
         @yield('content')
